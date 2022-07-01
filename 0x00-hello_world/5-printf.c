c3be90a6bb41df2d204c4d2f69ac8bacc4d726a7
Theo Madikgetla <68051817+Centric205@users.noreply.github.com>
2022-07-01 04:38:30 +0200

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Author: Theo Madikgetla
- *
+ * Hello, printf task
+ * Return: return 1 after program finishes execution
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
